4b4d11c4ccaf3f72ce91e0a73f83b5957cfb6fd5
Arth Parikshit Patel <arth200patel@gmail.com>
2026-09-03 18:13:18 +0530

    Add Terraform project

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    region = "us-east-1"  # Set your desired AWS region
+}
+
+resource "aws_instance" "example" {
+    ami           = "ami-0b6d9d3d33ba97d99"  # Specify an appropriate AMI ID
+    instance_type = "t3.micro"
+    subnet_id = "subnet-0b960b8bd157bcd73"
+    key_name = "project"
+}
